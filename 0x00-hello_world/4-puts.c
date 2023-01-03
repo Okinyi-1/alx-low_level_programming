d1cd9db470d27ab27a94f087ab726dccf80e5a5e
Okinyi1 <okinyitonnie346@gmail.com>
2023-01-02 23:57:48 -0500

    Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+
+/**
+ * main - print to screen          *
+ *
+ *
+ * Return: 0
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+
+}
+Footer
+
