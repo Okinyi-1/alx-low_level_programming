57875624ed0665b5a6d67ff07bb4ee0855df7856
Tonnie Okinyi <okinyio.tonnie@gmail.com>
2023-03-06 21:57:36 +0300

    Created Smile in the mirror, Hexadecimal, and Patience files

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main(void)
+{
+		int num1;
+
+			for (num1 = 0; num1 <= 9; num1++)
+					{
+								
+									putchar((num1 % 10) + '0');
+												
+												if (num1 == 9)
+																	continue;
+															
+															putchar(',');
+																		putchar(' ');
+																				}
+				
+
+				putchar('\n');
+
+					return (0);
+}
