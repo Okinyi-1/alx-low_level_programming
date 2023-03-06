77968712bc60b502749d6365da6b37da70caaf76
Tonnie Okinyi <okinyio.tonnie@gmail.com>
2023-03-06 22:14:40 +0300

    Created 0_putchar file

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,21 @@
+#include "stdio.h"
+
+/**
+ *  * _putchar - prints to stout
+ *   *
+ *    * Return; Always 0 (Sucess)
+ *     */
+int main(void)
+{
+	putchar('_');
+	putchar('p');
+	putchar('u');
+	putchar('t');
+	putchar('c');
+	putchar('h');
+	putchar('a');
+	putchar('r');
+	putchar('\n');
+
+	return (0);
+}
